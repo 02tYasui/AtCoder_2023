4577cb0178e509f3e129d5023c7db0f52865e7b6
Taichi Yasui <yasutai12@gmail.com>
2023-12-16 21:00:15 +0900

    commit

---
+++ contests/AtCoder Regular Contest 165/A - Sum equals LCM.py
@@ -0,0 +1,15 @@
+from math import lcm
+
+
+def main():
+    t, cases = map(int, input().split())
+
+    case = []
+
+    for _ in range(cases):
+        case.append(int(input()))
+
+
+    for num in case:
+        
+    lcm(cases)
